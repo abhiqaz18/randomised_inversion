df1a645a5a6adbbb36b25c699b5f5bbf33a67e7f
abhiqaz18 <>
2025-04-15 16:15:58 +0200

    Implemented Gower & Richtarik (2016) algo. 3.

--- main/matrix_inverters.py
+++ main/matrix_inverters.py
@@ -2,9 +2,15 @@ import numpy as np
 from scipy.stats import norm
 std_norm = norm(0, 1)
 
+# TO-DO
+# 1. Define convergence criterion, add max_iters, make iterations terminate automatically.
+# 2. Allow arbitrary m to be set (as frac).
+# 3. Allow arbitrary distribution to be set.
+# 4. Consider preloading sampled matrices. 
+
 def Gower_Richtarik_2016_1(A: np.matrix) -> np.matrix:
     """
-    Algorithm 1 from Gower and Richtárik (2016). 
+    Algorithm 1 from Gower and Richtárik (2016).
     Called Stochastic Iterative Matrix Inversion (SIMI) – nonsymmetric row variant.
     Parameters: distribution D and positive definite matrix W with the same dimensions as A.
     """
@@ -14,14 +20,28 @@ def Gower_Richtarik_2016_1(A: np.matrix) -> np.matrix:
     num_iters = 200
 
     A_inv = np.matrix(std_norm.rvs(size=(n, n)))
-    for i in range(num_iters):
+    for _ in range(num_iters):
         S = np.matrix(std_norm.rvs(size=(n, m)))
         L = S * np.matrix(np.linalg.inv(S.T * A * A.T * S)) * S.T
         A_inv += A.T * L * (I - A * A_inv)
 
     return A_inv
 
-if __name__ == '__main__':
-    A = np.matrix(std_norm.rvs(size=(10, 10)))
-    A_inv = Gower_Richtarik_2016_1(A)
-    print(np.linalg.eigvals(A * A_inv))
+def Gower_Richtarik_2016_3(A: np.matrix) -> np.matrix:
+    """
+    Algorithm 1 from Gower and Richtárik (2016).
+    Called Stochastic Iterative Matrix Inversion (SIMI) - symmetric variant.
+    Parameters: distribution D and positive definite matrix W with the same dimensions as A.
+    """
+    assert A == A.T, 'Please ensure input matrix is symmetric.' # Does this play well with floats?
+    n = A.shape[0]
+    I = np.matrix(np.eye())
+    m = n // 4
+    num_iters = 200
+
+    A_inv = np.matrix(std_norm.rvs(size=(n, n)))
+    A_inv = (A_inv + A_inv.T) / 2
+    for _ in range(num_iters):
+        S = 
+
+    return 0
